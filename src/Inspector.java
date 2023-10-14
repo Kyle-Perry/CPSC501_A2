233a147fae4215452d283ddbcc165fba6bce35a7
Kyle Perry <perry.246@gmail.com>
2023-10-14 15:59:09 -0600

    Field values working
    Non-recursive field values working.

--- src/Inspector.java
+++ src/Inspector.java
@@ -11,7 +11,7 @@ public class Inspector {
 		Constructor<?>[] constructors = classObject.getDeclaredConstructors();
 		Method[] methods = classObject.getDeclaredMethods();
 		Field[] fields = classObject.getDeclaredFields();
-		
+
 		System.out.println("Class Name = " + classObject.getName());
 		if(superClassObj != null) {
 			System.out.println("Superclass = " + superClassObj.getName());
@@ -23,7 +23,7 @@ public class Inspector {
 			}
 			System.out.println();
 		}
-		
+
 		if(constructors.length > 0)
 		{
 			System.out.println("Declared Constructors:");
@@ -71,14 +71,60 @@ public class Inspector {
 			}
 			System.out.println();
 		}
-		
+
 		if(fields.length > 0)
 		{
 			System.out.println("Declared Fields:");
 			for(Field f: fields) {
 				System.out.print(modifierString(f.getModifiers()));
 				System.out.print(f.getType().getName()+ " ");
-				System.out.print(f.getName());			
+				System.out.print(f.getName());
+				try{
+					f.setAccessible(true);
+					
+					Object fieldObject;
+					if(!Modifier.isStatic(f.getModifiers()))
+						fieldObject = f.get(obj);
+					else
+						fieldObject = f.get(null);
+						
+					System.out.print(" = ");
+
+					if(fieldObject != null ) {
+						if(fieldObject.getClass() == Integer.class) {
+							System.out.print(((Integer)fieldObject).intValue());
+						}
+						else if(fieldObject.getClass() == Double.class) {
+							System.out.print(((Double)fieldObject).doubleValue());
+						}	
+						else if(fieldObject.getClass() == Float.class) {
+							System.out.print(((Float)fieldObject).floatValue());
+						}
+						else if(fieldObject.getClass() == Byte.class) {
+							System.out.print(((Byte)fieldObject).byteValue());
+						}		
+						else if(fieldObject.getClass() == Short.class) {
+							System.out.print(((Short)fieldObject).shortValue());
+						}	
+						else if(fieldObject.getClass() == Long.class) {
+							System.out.print(((Long)fieldObject).longValue());
+						}		
+						else if(fieldObject.getClass() == Character.class) {
+							System.out.print(((Character)fieldObject).charValue());
+						}			
+						else if(fieldObject.getClass() == Boolean.class) {
+							System.out.print(((Boolean)fieldObject).booleanValue());
+						}				
+						else {
+							System.out.print(fieldObject.getClass().getName()+"@"+fieldObject.hashCode());
+						}
+					}
+					else
+						System.out.print("null");
+				}
+				catch(Exception e){
+					System.out.print(" !FAILED ACCESS!\n" + e.toString());
+				}
 				System.out.println();
 			}
 
@@ -112,5 +158,5 @@ public class Inspector {
 			modifiers += "volatile ";
 		return modifiers;
 	}
-	
+
 }
